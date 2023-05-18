2488442e7e7ea76074778f426ac57a791c15fbd8
johnahumanbeing <mseewak@gmail.com>
2023-05-18 22:26:54 +0300

    UNIX

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+ * main- prints stated quote
+ * Return: returns 1
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\"- Dora korpar, 2015-10-19\n" 59);
+	return (1);
+}
