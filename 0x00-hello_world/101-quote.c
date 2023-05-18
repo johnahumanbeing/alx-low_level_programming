4a5a8cbe2cfbac381f794aa8c71540a985cefd53
johnahumanbeing <mseewak@gmail.com>
2023-05-18 22:35:25 +0300

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <unistd.h>
 
 /**
- * main- prints stated quote
- * Return: returns 1
+ * main - Entry point
+ *
+ * Description: print a message to the standard error
+ *
+ * Return: 1 (Error)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\"- Dora korpar, 2015-10-19\n" 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
