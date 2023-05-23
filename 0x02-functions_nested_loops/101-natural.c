0d75c8f243e5cf7d4922fb6460cd0af4c85fe0f9
johnahumanbeing <mseewak@gmail.com>
2023-05-24 01:40:57 +0300

    nturall

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints the sum of all multiples of 3 or 5 up to 1024
+ * Return: Always Success
+ */
+int main(void)
+{
+	int i, sum;
+
+	sum = 0;
+	for (i = 0 ; i < 1024 ; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
