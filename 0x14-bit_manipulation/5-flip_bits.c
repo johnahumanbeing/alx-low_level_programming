76755d45e406615790998d1402a1ff8f8c956816
johnahumanbeing <mseewak@gmail.com>
2023-08-31 22:26:55 +0300

    crackme3 pass

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns number of bits needed to flip from one number to another
+ * @n: the giving number
+ * @m: second number
+ *
+ * Return: number of bits needed to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+}
