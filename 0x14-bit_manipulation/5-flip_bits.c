11afdbe3ceec543ad1f10bea14b8917fb71ab819
johnahumanbeing <mseewak@gmail.com>
2023-08-31 23:43:00 +0300

    a function that checks the endianness

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,4 +9,14 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+	unsigned long int bits = n ^ m;
+	unsigned int c = 0;
+
+	/* calculating the XOR of the two numbers to get the bits that are diff */
+	while (bits != 0)
+	{
+		c += bits & 1;
+		bits >>= 1;
+	}
+	return (c);
 }
